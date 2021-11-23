4ea332ab5fabeaa4c668abb494bd13d870690a97
Vadim <korobeinikovv@mail.ru>
2021-11-23 22:58:32 +0300

    Get db services and client

--- src/clients/db.js
+++ src/clients/db.js
@@ -1,7 +1,28 @@
 import PouchDB from 'pouchdb'
 
-const db = new PouchDB('http://localhost:5984/notes')
+const localDB = new PouchDB('notes')
+const remoteDb = new PouchDB('http://localhost:5984/notes')
 
 export const getNotesDb = () => {
-  return db
+  return localDB
+}
+
+export const sync = () => {
+  localDB
+    .sync(remoteDb, {
+      live: true,
+      retry: true,
+    })
+    .on('change', function (change) {
+      // something changed!
+    })
+    .on('paused', function (info) {
+      // replication was paused, usually because of a lost connection
+    })
+    .on('active', function (info) {
+      // replication was resumed
+    })
+    .on('error', function (err) {
+      // totally unhandled error (shouldn't happen)
+    })
 }
--- src/services/notes.js
+++ src/services/notes.js
@@ -1,9 +1,32 @@
 import { getNotesDb } from '../clients/db'
+import { getGuid } from '../utils/guid'
 
 export const getInfo = () => {
   return getNotesDb()
     .info()
     .then((info) => {
       console.log(info)
+    })
+}
+
+export const putNote = ({ title, text }) => {
+  const doc = {
+    _id: getGuid(),
+    title,
+    text,
+    // createdAt
+  }
+  return getNotesDb().put(doc)
+}
+
+export const getNote = ({ id }) => {
+  return getNotesDb().get(id)
+}
+
+export const removeNote = ({ id }) => {
+  return getNotesDb()
+    .get(id)
+    .then((doc) => {
+      return getNotesDb().remove(doc)
     })
 }
